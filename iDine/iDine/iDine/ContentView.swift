0d8e76b2561f4933f44f1df9c9eb64a24de0c7fd
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2019-07-08 17:29:57 +0100

    Initial iDine App

---
+++ iDine/iDine/iDine/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Chris Thomas on 08/07/2019.
+//  Copyright © 2019 Chris Thomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif

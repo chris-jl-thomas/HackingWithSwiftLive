30d39aceaf551cd24ebfa837ceab1bc25edb2317
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2019-07-09 11:07:50 +0100

    Add an order screen and checlout placeholder

---
+++ iDine/iDine/iDine/OrderView.swift
@@ -0,0 +1,46 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Chris Thomas on 09/07/2019.
+//  Copyright © 2019 Chris Thomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView : View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink(destination: Text("Check out")) {
+                        Text("Place order")
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Order"))
+            .listStyle(.grouped)
+        }
+    }
+}
+
+#if DEBUG
+struct OrderView_Previews : PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        order.add(item: MenuItem.example)
+        return OrderView().environmentObject(order)
+    }
+}
+#endif

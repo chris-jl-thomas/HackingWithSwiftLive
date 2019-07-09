e2973e50b4edd82c36e45a9390a332835f4e626d
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2019-07-09 12:09:29 +0100

    Added the checkout page with dynamic tips and pricing

--- iDine/iDine/iDine/CheckoutView.swift
+++ iDine/iDine/iDine/CheckoutView.swift
@@ -12,8 +12,19 @@ struct CheckoutView : View {
     @EnvironmentObject var order: Order
     
     static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    static let tipAmounts = [10, 15, 20, 25, 0]
     
-    @State var paymentType = 0
+    var totalPrice: Double {
+        
+        let total = Double(order.total)
+        let tipValue = (total / 100) * Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
+    
+    @State private var paymentType = 0
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 1
     
     var body: some View {
         Form {
@@ -22,6 +33,33 @@ struct CheckoutView : View {
                     ForEach(0 ..< Self.paymentTypes.count) {
                         Text(Self.paymentTypes[$0])
                     }
+                }
+                
+                Toggle(isOn: $addLoyaltyDetails) {
+                    Text("Add iDine loyalty card")
+                }
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                
+                SegmentedControl(selection: $tipAmount) {
+                    
+                    ForEach(0 ..< Self.tipAmounts.count) {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }
+            }
+            
+            Section(header: Text("Total: $\(totalPrice, specifier: "%.2f")")) {
+                
+                Button(action: {
+                    // Do Something
+                }) {
+                    Text("Confirm order")
                 }
             }
         }

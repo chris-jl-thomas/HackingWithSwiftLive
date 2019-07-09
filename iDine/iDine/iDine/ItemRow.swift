7b9eba696974c5eb36322fe74324bf4e8d458cc9
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2019-07-09 10:14:08 +0100

    Up to creating a detail screen

--- iDine/iDine/iDine/ItemRow.swift
+++ iDine/iDine/iDine/ItemRow.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct ItemRow: View {
     
+    static let colors: [String: Color]  = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
+    
     var item: MenuItem
     
     var body: some View {
@@ -17,10 +19,25 @@ struct ItemRow: View {
         HStack {
             
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
             
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }
+            
+            Spacer()
+            
+            ForEach(item.restrictions.identified(by: \.self)) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(Self.colors[restriction]!)
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
